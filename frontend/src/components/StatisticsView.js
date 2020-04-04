4f4f19027ced554753ce4a07af3e56c09e8b17b2
Thomas Kalnik <kalnik.thomas@gmail.com>
2020-04-04 16:24:01 -0600

    add statsview dropdown component

--- frontend/src/components/StatisticsView.js
+++ frontend/src/components/StatisticsView.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import Navigation from './Navigation';
 
 class StatisticsView extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          dropdownOpen: true
+        };
+      }
+    
+      toggle() {
+        this.setState(prevState => ({
+          dropdownOpen: !prevState.dropdownOpen
+        }));
+      }
+
     render() {
         return (
+            <div>
             <div class="container-fluid">
-                <Navigation/>
+                <Navigation />
+            </div>
+            <div>
+                <Dropdown isOpen={this.state.dropdownOpen} size="lg" toggle={this.toggle}>
+                    <DropdownToggle caret>
+                        Statistics Menu
+        </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem header>Select Statistic</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>Statistic 1</DropdownItem>
+                        <DropdownItem>Statistic 2</DropdownItem>
+                        <DropdownItem>Statistic 3</DropdownItem>
+                        <DropdownItem>Statistic 4</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
             </div>
         );
     }
