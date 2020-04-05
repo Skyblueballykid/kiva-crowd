d66ac1eccf55bb9d5180da5a64ba6dd0745bd48c
Thomas Kalnik <kalnik.thomas@gmail.com>
2020-04-04 22:51:24 -0600

    add statistics endpoint

--- backend/local_app/kiva/statistics.py
+++ backend/local_app/kiva/statistics.py
@@ -1,4 +1,25 @@
-from django.db import connection
+from django.db import connection, models
 
 from .models import Loan, Lender
+
+class LoanStats(object):
+    '''Performs the following raw SQL queries on the Loans model:
+    1. get_all_loans selects all rows and columns from the Loans table 
+    '''
+    
+    def get_all_loans(self):
+        query = 'SELECT * FROM loan LIMIT 100'
+        return query
+
+
+class LenderStats(object):
+    '''Performs the following raw SQL queries on the Lenders model:
+    1. get_all_lenders selects all rows and columns from the Lenders table
+    '''
+
+    def get_all_lenders(self):
+        query = 'SELECT * FROM Lender LIMIT 100'
+        return query
+
+
 
