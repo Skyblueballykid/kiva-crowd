e70cec7a5b15bf4d814da84dcf58317f256412df
Trung Nguyen <trung@kelleroffers.com>
2020-04-06 15:06:28 -0500

    set mode no-cors

--- frontend/src/api/LenderApi.js
+++ frontend/src/api/LenderApi.js
@@ -2,7 +2,7 @@ const { REACT_APP_API } = process.env;
 
 export default {
     getLenders() {
-        return fetch(`${REACT_APP_API}/api/lender/`)
+        return fetch(`${REACT_APP_API}/api/lender/`, {mode:'no-cors'})
             .then(res => res.json())
             .then(
                 (data) => {
