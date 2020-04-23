b7f7348d7f1dd5ab5677b5ed19c555bfb1e6f2ec
skyripley <skylar.ripley@favordelivery.com>
2020-04-23 17:14:29 -0500

    Initial loan text search endpoint

--- backend/local_app/kiva/urls.py
+++ backend/local_app/kiva/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import LenderList, LenderDetail, LoanList, LoanDetail, Stats_1List,Stats_2List,Stats_3List
+from .views import LenderList, LenderDetail, LoanList, LoanDetail, Stats_1List, Stats_2List, Stats_3List, LoanListSearch
 
 
 urlpatterns = [
     path('lender/', LenderList.as_view()),
     path('lender/<slug:pk>/', LenderDetail.as_view()),
     path('loan/', LoanList.as_view()),
+    path('loan/search/', LoanListSearch.as_view()),
     path('loan/<slug:pk>/', LoanDetail.as_view()),
     path('statistics/stats_1', Stats_1List.as_view()),
     path('statistics/stats_2', Stats_2List.as_view()),
