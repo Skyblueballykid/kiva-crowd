5e910b927137a1d927a28addd4b644a4f826dee7
skyripley <skylar.ripley@favordelivery.com>
2020-05-02 13:07:57 -0500

    Add indexes and constraints

--- backend/local_app/kiva/models.py
+++ backend/local_app/kiva/models.py
@@ -9,12 +9,12 @@ class Lender(models.Model):
     city = models.TextField(blank=True, null=True)
     state = models.CharField(max_length=50, blank=True, null=True)
     country_code = models.CharField(max_length=2, blank=True, null=True)
-    member_since = models.DateTimeField(blank=True, null=True)
+    member_since = models.DateTimeField(blank=True, null=True, db_index=True)
     personal_url = models.CharField(max_length=300, blank=True, null=True)
     occupation = models.CharField(max_length=200, blank=True, null=True)
     loan_because = models.TextField(blank=True, null=True)
     other_info = models.TextField(blank=True, null=True)
-    loan_purchase_num = models.IntegerField()
+    loan_purchase_num = models.IntegerField(db_index=True)
     invited_by = models.CharField(max_length=50, blank=True, null=True)
     num_invited = models.IntegerField()
 
@@ -46,8 +46,8 @@ class Loan(models.Model):
     original_language = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     description_translated = models.TextField(blank=True, null=True)
-    funded_amount = models.FloatField(blank=True, null=True)
-    loan_amount = models.FloatField(blank=True, null=True)
+    funded_amount = models.FloatField(blank=True, null=True, db_index=True)
+    loan_amount = models.FloatField(blank=True, null=True, db_index=True)
     status = models.CharField(max_length=50, blank=True, null=True)
     image_id = models.CharField(max_length=10, blank=True, null=True)
     video_id = models.CharField(max_length=10, blank=True, null=True)
@@ -61,12 +61,12 @@ class Loan(models.Model):
     currency_exchange_coverage_rate = models.FloatField(blank=True, null=True)
     currency = models.CharField(max_length=3, blank=True, null=True)
     partner_id = models.CharField(max_length=10, blank=True, null=True)
-    posted_time = models.DateTimeField(blank=True, null=True)
+    posted_time = models.DateTimeField(blank=True, null=True, db_index=True)
     planned_expiration_time = models.DateTimeField(blank=True, null=True)
-    disburse_time = models.DateTimeField(blank=True, null=True)
-    raised_time = models.DateTimeField(blank=True, null=True)
+    disburse_time = models.DateTimeField(blank=True, null=True, db_index=True)
+    raised_time = models.DateTimeField(blank=True, null=True, db_index=True)
     lender_term = models.IntegerField(blank=True, null=True)
-    num_lenders_total = models.IntegerField(blank=True, null=True)
+    num_lenders_total = models.IntegerField(blank=True, null=True, db_index=True)
     num_journal_entries = models.IntegerField(blank=True, null=True)
     num_bulk_entries = models.IntegerField(blank=True, null=True)
     tags = models.TextField(blank=True, null=True)
@@ -84,6 +84,10 @@ class Loan(models.Model):
 
     class Meta:
         db_table = 'loan'
+        constraints = [
+            models.CheckConstraint(check=models.Q(loan_amount__gt=0), name='loan_amount_gt_0'),
+            models.CheckConstraint(check=models.Q(funded_amount__gte=0), name='funded_amount_gte_0')
+        ]
 
 
 class LoanStatsAvgLoanByCountry(models.Model):
