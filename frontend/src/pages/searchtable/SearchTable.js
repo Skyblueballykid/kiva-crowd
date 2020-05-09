2e81bb4dc9d58809333c38b2038699c97ffdf632
skyripley <skylar.ripley@favordelivery.com>
2020-05-09 13:02:32 -0500

    Update indentation in SearchTable.js

--- frontend/src/pages/searchtable/SearchTable.js
+++ frontend/src/pages/searchtable/SearchTable.js
@@ -14,138 +14,138 @@ import COLUMNS from './SearchTable.columns';
 const { REACT_APP_API } = process.env;
 
 const SearchTable = (props) => {
-    const [form] = Form.useForm();
-    const [data, setData] = useState([]);
-    const [editingKey, setEditingKey] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+  const [data, setData] = useState([]);
+  const [editingKey, setEditingKey] = useState('');
+  const [loading, setLoading] = useState(false);
 
 
 useEffect(() => {
-    let query = "";
-    if (props.location.state && props.location.state.query) {
-      query = `?search=${props.location.state.query}`;
-    }
-    const fetchData = async () => {
-        setLoading(true);
-        const res = await axios.get(`${REACT_APP_API}/api/loan/search${query}`);
-        const { results } = res.data;
-        console.log(results);
-        setData(results);
-        setLoading(false);
-    };
-    fetchData();
-    }, [props.location.state]);
+  let query = "";
+  if (props.location.state && props.location.state.query) {
+    query = `?search=${props.location.state.query}`;
+  }
+  const fetchData = async () => {
+    setLoading(true);
+    const res = await axios.get(`${REACT_APP_API}/api/loan/search${query}`);
+    const { results } = res.data;
+    console.log(results);
+    setData(results);
+    setLoading(false);
+  };
+  fetchData();
+  }, [props.location.state]);
   
 
   const isEditing = (record) => record.id === editingKey;
 
   const edit = (record) => {
-    form.setFieldsValue({ ...record });
-    setEditingKey(record.id);
+  form.setFieldsValue({ ...record });
+  setEditingKey(record.id);
   };
 
   const cancel = () => {
-    setEditingKey('');
+  setEditingKey('');
   };
   
   const save = async (id) => {
-    try {
-      const row = await form.validateFields();
-      const newData = [...data];
-      const index = newData.findIndex((item) => id === item.id);
+  try {
+    const row = await form.validateFields();
+    const newData = [...data];
+    const index = newData.findIndex((item) => id === item.id);
 
-      if (index > -1) {
-        const item = newData[index];
-        newData.splice(index, 1, { ...item, ...row });
-        setData(newData);
-      } else {
-        newData.push(row);
-        setData(newData);
-      }
-    } catch (errInfo) {
-      console.log('Validate Failed:', errInfo);
+    if (index > -1) {
+    const item = newData[index];
+    newData.splice(index, 1, { ...item, ...row });
+    setData(newData);
+    } else {
+    newData.push(row);
+    setData(newData);
     }
+  } catch (errInfo) {
+    console.log('Validate Failed:', errInfo);
+  }
   };
-    
-const columns = [
-  ...COLUMNS,
-  {
+  
+  const columns = [
+    ...COLUMNS,
+    {
     title: 'Actions',
     dataIndex: 'action',
     render: (_, record) => {
       const editable = isEditing(record);
       return editable ? (
-        <span>
-          <a
-            href="javascript:;"
-            onClick={() => save(record.id)}
-            style={{
-              marginRight: 8,
-            }}
-          >
-            Save
-          </a>
-          <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-            <a>Cancel</a>
-          </Popconfirm>
-        </span>
-      ) : (
-        <a disabled={editingKey !== ''} onClick={() => edit(record)}>
-          Edit
+      <span>
+        <a
+        href="javascript:;"
+        onClick={() => save(record.id)}
+        style={{
+          marginRight: 8,
+        }}
+        >
+        Save
         </a>
+        <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+        <a>Cancel</a>
+        </Popconfirm>
+      </span>
+      ) : (
+      <a disabled={editingKey !== ''} onClick={() => edit(record)}>
+        Edit
+      </a>
       );
     },
-  },
-];
-    
-      const mergedColumns = columns.map((col) => {
-        if (!col.editable) {
-          return col;
-        }
-    
-        return {
-          ...col,
-          onCell: (record) => ({
-            record,
-            inputType: col.dataIndex === 'age' ? 'number' : 'text',
-            dataIndex: col.dataIndex,
-            title: col.title
-          }),
-        };
-      });
-    
-      return (
-        <StyledDiv>
-          <Form form={form} component={false}>
-            <Table
-              scroll={{ x: 1900 }}
-              components={{
-                body: {
-                  cell: EditableCell,
-                },
-              }}
-              bordered
-              dataSource={data}
-              columns={mergedColumns}
-              rowClassName="editable-row"
-              pagination={{
-                defaultPageSize: 20,
-                showSizeChanger: false,
-              }}
-              loading={loading}
-            />
-          </Form>
-        </StyledDiv>
-      );
-    };
+    },
+  ];
+  
+  const mergedColumns = columns.map((col) => {
+  if (!col.editable) {
+    return col;
+  }
 
-    SearchTable.propTypes = {
-      location: PropTypes.shape({
-        state: PropTypes.shape({
-          query: PropTypes.string
-        })
-      }).isRequired
-    };
-    
-    export default SearchTable;
-    
+  return {
+    ...col,
+    onCell: (record) => ({
+    record,
+    inputType: col.dataIndex === 'age' ? 'number' : 'text',
+    dataIndex: col.dataIndex,
+    title: col.title
+    }),
+  };
+  });
+
+  return (
+  <StyledDiv>
+    <Form form={form} component={false}>
+    <Table
+      scroll={{ x: 1900 }}
+      components={{
+      body: {
+        cell: EditableCell,
+      },
+      }}
+      bordered
+      dataSource={data}
+      columns={mergedColumns}
+      rowClassName="editable-row"
+      pagination={{
+      defaultPageSize: 20,
+      showSizeChanger: false,
+      }}
+      loading={loading}
+    />
+    </Form>
+  </StyledDiv>
+  );
+};
+
+SearchTable.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      query: PropTypes.string
+    })
+  }).isRequired
+};
+  
+export default SearchTable;
+  
