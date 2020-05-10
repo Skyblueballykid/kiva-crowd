9a5af9de853fc1c267ac5a9b3e95dabc3b3c990f
Thomas Kalnik <kalnik.thomas@gmail.com>
2020-05-09 21:47:07 -0500

    add the analysis page and some styling

--- frontend/src/pages/analysis/Analysis.js
+++ frontend/src/pages/analysis/Analysis.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import TOP_5_COUNTRY_LOANS_BAR from './charts/TOP_5_COUNTRY_LOANS_BAR.svg';
+import TOP_5_SECTOR_LOANS_BAR from './charts/TOP_5_SECTOR_LOANS_BAR.svg';
+import TOP_5_COUNTRY_LOANS_DETAIL from './charts/TOP_5_COUNTRY_LOANS_DETAIL.svg';
+import TOP_5_SECTOR_LOANS_DETAIL from './charts/TOP_5_SECTOR_LOANS_DETAIL.svg';
 
 const StyledDiv = styled.div`
   min-height: 88vh;
@@ -8,7 +12,15 @@ const StyledDiv = styled.div`
 const Analysis = () => {
   return (
     <StyledDiv>
-      <h2>Analysis</h2>
+      <h4><b>Analysis of Top Loans by Country and Sector</b></h4>
+
+      <img src={ TOP_5_COUNTRY_LOANS_BAR } alt="TOP_5_COUNTRY" />
+      <img src= { TOP_5_SECTOR_LOANS_BAR } alt="TOP_5_SECTOR" />
+      <br />
+
+      <h4><b>Detailed Analysis of Top 5 Countries and Sectors</b></h4>
+      <img src= { TOP_5_COUNTRY_LOANS_DETAIL } alt="TOP_5_COUNTRY_DETAIL" />
+      <img src= { TOP_5_SECTOR_LOANS_DETAIL } alt="TOP_5_COUNTRY_DETAIL" />
     </StyledDiv>
   );
 };
